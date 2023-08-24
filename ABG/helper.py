09f56b6bd46f8e1312bf6d201f7528bd9c3470a4
HERO OF VILLAGE <118505934+JaiHansda@users.noreply.github.com>
2023-08-24 09:54:47 +0530

    Update helper.py

--- ABG/helper.py
+++ ABG/helper.py
@@ -1,6 +1,6 @@
-HELP_IMG = ["https://telegra.ph/file/2ad7c9d508b26c3cc7c09.jpg"]
+HELP_IMG = ["https://telegra.ph/file/af47c67f441f58b0e260c.jpg"]
 
-START_IMG = ["https://telegra.ph/file/2ad7c9d508b26c3cc7c09.jpg"]
+START_IMG = ["https://telegra.ph/file/78fb5f72eed2924496fba.jpg"]
 
 
-PHOTO = ["https://telegra.ph/file/2ad7c9d508b26c3cc7c09.jpg"]
+PHOTO = ["https://telegra.ph/file/78fb5f72eed2924496fba.jpg"]
